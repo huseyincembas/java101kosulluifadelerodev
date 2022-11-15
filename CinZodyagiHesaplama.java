ab8254589b7515175085fb8c426d450e2cd4f806
Hüseyin Cem <95631429+huseyincembas@users.noreply.github.com>
2022-11-15 16:34:41 +0300

    Add files via upload

---
+++ CinZodyagiHesaplama.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class CinZodyagiHesaplama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int birthYear, zodiacSign;
+        System.out.print("Doğum Yılınızı Giriniz: ");
+        birthYear = input.nextInt();
+        zodiacSign = birthYear%12;
+
+        switch (zodiacSign){
+            case 0:
+                System.out.print("Çin Zodyağı Burcunuz: Maymun");
+                break;
+            case 1:
+                System.out.print("Çin Zodyağı Burcunuz: Horoz");
+                break;
+            case 2:
+                System.out.print("Çin Zodyağı Burcunuz: Köpek");
+                break;
+            case 3:
+                System.out.print("Çin Zodyağı Burcunuz: Domuz");
+                break;
+            case 4:
+                System.out.print("Çin Zodyağı Burcunuz: Fare");
+                break;
+            case 5:
+                System.out.print("Çin Zodyağı Burcunuz: Öküz");
+                break;
+            case 6:
+                System.out.print("Çin Zodyağı Burcunuz: Kaplan");
+                break;
+            case 7:
+                System.out.print("Çin Zodyağı Burcunuz: Tavşan");
+                break;
+            case 8:
+                System.out.print("Çin Zodyağı Burcunuz: Ejderha");
+                break;
+            case 9:
+                System.out.print("Çin Zodyağı Burcunuz: Yılan");
+                break;
+            case 10:
+                System.out.print("Çin Zodyağı Burcunuz: At");
+                break;
+            default:
+                System.out.print("Çin Zodyağı Burcunuz: Koyun");
+                break;
+        }
+    }
+}
